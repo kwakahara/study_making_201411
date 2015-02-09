cae2891f9f30f975114f1a3d7aba2f5a175729b8
kwakahara <k_wakahara@ktsystem.jp>
2015-02-09 23:17:29 +0900

    整形を行い、インデントを修正しました　#8

--- Kadai201411/src/jp/ktsystem/kadai201411/k_wakahara/CheckAndReadFile.java
+++ Kadai201411/src/jp/ktsystem/kadai201411/k_wakahara/CheckAndReadFile.java
@@ -28,33 +28,36 @@ public class CheckAndReadFile {
 
 	/**
 	 * フォルダの中身をチェックするメソッド
-	 * @param anOrderFileDir チェックするフォルダパス
+	 * 
+	 * @param anOrderFileDir
+	 *            チェックするフォルダパス
 	 * @return フォルダ内のテキストファイルの中身のリスト
 	 * @throws KadaiException
 	 */
-	public static List<List<String>> checkFiles(String anOrderFileDir) throws KadaiException {
-		
+	public static List<List<String>> checkFiles(String anOrderFileDir)
+			throws KadaiException {
+
 		// 引数nullチェック
-		if(null == anOrderFileDir){
+		if (null == anOrderFileDir) {
 			throw new KadaiException(ErrorCode.SALES_ORDER_FILE_INPUT);
 		}
 
 		File dir = new File(anOrderFileDir);
 		File[] files = dir.listFiles();
 		List<List<String>> returnTextList = new ArrayList<List<String>>();
-		if(null == files){
+		if (null == files) {
 			throw new KadaiException(ErrorCode.SALES_ORDER_FILE_INPUT);
 		}
-		
-		try{
-		for (int i = 0; i < files.length; i++) {
-			// フォルダの中身をチェック
-			String filePath = files[i].toString();
-			if (KadaiUtil.checkSuffix(filePath, SUFFIX)) {
-				// ファイルの拡張子が「txt」の場合のみ読みこみ処理を実行
-				returnTextList.add(KadaiUtil.readFile(filePath, ENCODING));
+
+		try {
+			for (int i = 0; i < files.length; i++) {
+				// フォルダの中身をチェック
+				String filePath = files[i].toString();
+				if (KadaiUtil.checkSuffix(filePath, SUFFIX)) {
+					// ファイルの拡張子が「txt」の場合のみ読みこみ処理を実行
+					returnTextList.add(KadaiUtil.readFile(filePath, ENCODING));
+				}
 			}
-		}
 		} catch (IOException e) {
 			throw new KadaiException(ErrorCode.SALES_ORDER_FILE_INPUT);
 		}
